73f2ea1a94e421beba9d51bf7be2ba0c765938af
NikkiMounce <nikkimounce@gmail.com>
2021-11-03 09:06:10 -0700

    deleted webpack file

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-const config = {
-    entry: "./routes/api.js",
-    output: {
-        path: __dirname + "/dist",
-        filename: "bundle.js"
-    },
-    mode: "development"
-};
-module.exports = config;
